2208486ccadbbef1a60744d33ccde8e179795f24
unknown <luisocara.luis@gmail.com>
2025-02-07 21:58:45 -0300

    config mongo docker

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/bhut');
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://mongo:27017/bhut');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error}`);
